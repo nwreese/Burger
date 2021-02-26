8859e07f1f9b2fdf915281cdfdc74a71c1094d4d
Nick Reese <nwreese@kent.edu>
2021-02-26 10:05:50 -0500

    setting up file structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Tacos');
+INSERT INTO burgers (burger_name) VALUES ('Cheddar Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza', true);
