f2fa3b16028c7c6c1a1ce1f1540147e4ce24a7e0
Nick Reese <nwreese@kent.edu>
2021-02-28 13:44:00 -0500

    updating handlebars

---
+++ public/js/burgers.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+      console.info('DOM loaded');
+    }
+
+    const changeDevourBtns = document.querySelectorAll('.change-devour');
+  
+    if (changeDevourBtns) {
+      changeDevourBtns.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          const id = e.target.getAttribute('data-id');
+          const newDevour = e.target.getAttribute('data-newdevour');
+  
+          const newDevourState = {
+            devoured: newDevour,
+          };
+  
+          fetch(`/api/burgers/${id}`, {
+            method: 'PUT',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+  
+            body: JSON.stringify(newDevourState),
+          }).then((response) => {
+            if (response.ok) {
+              console.log(`changed devour to: ${newDevour}`);
+              location.reload('/');
+            } else {
+              alert('something went wrong!');
+            }
+          });
+        });
+      });
+    }
+  
+    const createBurgerBtn = document.getElementById('create-form');
+  
+    if (createBurgerBtn) {
+      createCatBtn.addEventListener('submit', (e) => {
+        e.preventDefault();
+  
+        const newBurger = {
+          burger_name: document.getElementById('ca').value.trim(),
+          devoured: document.getElementById('devoured').checked,
+        };
+  
+        fetch('/api/burgers', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+  
+          body: JSON.stringify(newBurger),
+        }).then(() => {
+          document.getElementById('ca').value = '';
+
+          console.log('Created a new burger!');
+          location.reload();
+        });
+      });
+    }
+  
+    const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
+
+    deleteBurgerBtns.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        const id = e.target.getAttribute('data-id');
+  
+        fetch(`/api/burgers/${id}`, {
+          method: 'DELETE',
+        }).then((res) => {
+          console.log(res);
+          console.log(`Deleted burger: ${id}`);
+  
+          location.reload();
+        });
+      });
+    });
+  });
+  
