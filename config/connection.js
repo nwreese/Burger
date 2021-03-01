ec11304aea4d17cd7f87bbee5c618c8a9381bb16
Nick Reese <nwreese@kent.edu>
2021-03-01 15:12:30 -0500

    updating jawdb

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,23 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.db_password,
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.db_password,
+    database: 'burgers_db'
+  })
+}
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: process.env.db_password,
+//   database: 'burgers_db',
+// });
 
 connection.connect((err) => {
   if (err) {
